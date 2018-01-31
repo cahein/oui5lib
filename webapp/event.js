2e2970ae61c8abc99664bad83d9bd0e9d76a76c5
Carsten Heinrigs <carsten@cahein.de>
2018-01-31 14:57:10 +0100

    Core or Component event

--- webapp/event.js
+++ webapp/event.js
@@ -1,12 +1,6 @@
 /** @namespace oui5lib.event */
 (function (logger, util) {
-    /**
-     * Handler called when request failed.
-     * @memberof oui5lib.event
-     */
-    function handleRequestFailure(channel, eventId, requestInfo) {
-        logger.error(eventId + " " + requestInfo.xhrObj);
-    }
+    const event = oui5lib.namespace("event");
 
     /**
      * Publish event in case of an error.
@@ -14,30 +8,36 @@
      * @param {string} eventId One of 'status', 'error', 'timeout'.
      * @param {object} props
      */
-    function publishRequestFailureEvent(eventId, xhr, props) {
+    function publishRequestFailureEvent(eventId, xhr, props, isComponentEvent) {
         if (typeof props === "undefined" || props === null) {
             props = {};
         }
         props.xhrObj = xhr;
-        publishEvent("xhr", eventId, props);
+        publishEvent("xhr", eventId, props, isComponentEvent);
     }
+    event.publishRequestFailureEvent = publishRequestFailureEvent;
     
-    function publishReadyEvent(eventData) {
-        publishEvent("loading", "ready", eventData);
+    function publishReadyEvent(eventData, isComponentEvent) {
+        publishEvent("loading", "ready", eventData, isComponentEvent);
     }
+    event.publishReadyEvent = publishReadyEvent;
 
-    function publishEvent(channelId, eventId, eventData) {
+
+    function publishEvent(channelId, eventId, eventData, isComponentEvent) {
         if (!util.isUI5Loaded()) {
             logger.warn("Couldn't publish event: no UI5 loaded");
             return;
         }
-        let eventBus = sap.ui.getCore().getEventBus();
+        if (typeof isComponentEvent !== "boolean") {
+            isComponentEvent = false;
+        }
+        let eventBus;
+        if (isComponentEvent) {
+            eventBus = util.getComponentEventBus();
+        } else {
+            eventBus = sap.ui.getCore().getEventBus();
+        }
         eventBus.publish(channelId, eventId, eventData);
     }
     
-    let event = oui5lib.namespace("event");
-    event.onRequestFailure = handleRequestFailure;
-    
-    event.publishRequestFailureEvent = publishRequestFailureEvent;
-    event.publishReadyEvent = publishReadyEvent;
 }(oui5lib.logger, oui5lib.util));
