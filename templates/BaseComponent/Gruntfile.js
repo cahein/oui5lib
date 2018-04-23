e77444d30e9bbb99cd547b5fd8f8f1b14ea37799
Carsten Heinrigs <carsten@cahein.de>
2018-04-23 09:16:26 +0200

    normalization

--- templates/BaseComponent/Gruntfile.js
+++ templates/BaseComponent/Gruntfile.js
@@ -119,7 +119,8 @@ module.exports = function (grunt) {
                             "lib/**"
                         ]
                     },
-                    dest: "<%= dirs.build %>/webapp"
+                    dest: "<%= dirs.build %>/webapp",
+                    compress: false
                 },
                 components: "ooooo"
             }
